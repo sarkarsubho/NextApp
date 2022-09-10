15cf98ba38bfbeac520cecd49e1f04bc06d14ba1
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-10 20:00:00 +0530

    fallback

--- nextapp/pages/posts/[postId].js
+++ nextapp/pages/posts/[postId].js
@@ -15,36 +15,46 @@ function Post({post}){
 
 export default Post
 export async function getStaticPaths (){
+    const response =await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const data =await response.json();
+    const paths=data.map((e)=>{
+        return{
+            params:{
+                postId: `${e.id}`
+            }
+        }
+    })
   return {
-    paths:[
-        {
-            params:{postId: "1"}
-        },
-        {
-            params:{postId: "2"}
-        },
-        {
-            params:{postId: "3"}
-        },
-        {
-            params:{postId: "4"}
-        },
-        {
-            params:{postId: "5"}
-        },
-        {
-            params:{postId: "6"}
-        },
-        {
-            params:{postId: "7"}
-        },
-        {
-            params:{postId: "8"}
-        },
-        {
-            params:{postId: "9"}
-        },
-    ],
+    // paths:[
+    //     {
+    //         params:{postId: "1"}
+    //     },
+    //     {
+    //         params:{postId: "2"}
+    //     },
+    //     {
+    //         params:{postId: "3"}
+    //     },
+    //     {
+    //         params:{postId: "4"}
+    //     },
+    //     {
+    //         params:{postId: "5"}
+    //     },
+    //     {
+    //         params:{postId: "6"}
+    //     },
+    //     {
+    //         params:{postId: "7"}
+    //     },
+    //     {
+    //         params:{postId: "8"}
+    //     },
+    //     {
+    //         params:{postId: "9"}
+    //     },
+    // ],
+    paths,
     fallback: false,
   }
 }
