2946e2b2eb22cfcfb3963e9cff56d1bcef6c20f5
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-17 04:15:04 +0530

    api update

--- nextapp/pages/api/index.js
+++ nextapp/pages/api/index.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'subhankar sarkar' })
-  
+  res.status(200).json({ name: 'subhankar sarkar ok' })
+
 }
